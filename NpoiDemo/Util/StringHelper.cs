b915d464ea13b619f900927fd80aec7a3c334b1c
TTSC <wusuoru@users.noreply.github.com>
2017-12-25 15:17:47 +0800

    StringHelper
    Get Base64String by fileStream

---
+++ NpoiDemo/Util/StringHelper.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpoiDemo.Util
+{
+    public class StringHelper
+    {
+        public static string GetBase64String(FileStream fs)
+        {
+            int count = 200;
+            int offset = 0;
+            List<byte> data = new List<byte>();
+            byte[] array = new byte[count];
+            while (fs.Read(array, offset, count) > 0)
+            {
+                data.AddRange(array);
+            }
+            string base64Str = Convert.ToBase64String(data.ToArray());
+
+            array = null;
+            data.Clear();
+
+            return base64Str;
+        }
+    }
+}
